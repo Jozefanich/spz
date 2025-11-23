61910dd598c1db8b82b692bc33935d0f349c0b2c
Jozefanich <114819622+Jozefanich@users.noreply.github.com>
2025-11-23 20:33:09 +0200

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <windows.h>
+int main()
+{
+    HANDLE res = CreateFile(L"\\\\.\\EXAMPLE", GENERIC_READ,0,nullptr,OPEN_EXISTING,0,nullptr);
+    if (res == INVALID_HANDLE_VALUE) {
+        std::cout << GetLastError();
+        return 1;
+    }
+    CloseHandle(res);
+    std::cout << "Hello World!\n";
+}
